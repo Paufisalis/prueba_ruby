f7a70a35f1940669fef9582c3e22b0f7df49d492
paulina <paulinafs89@gmail.com>
2019-08-21 12:41:45 -0400

    four Commit

--- prueba.rb
+++ prueba.rb
@@ -30,12 +30,28 @@ def absent_student
       sum += 1 if absent == "A" #agrego la operacion suma, en cada vuelta suma 1,  si encuentra un elemento que sea "A" sume 1
     end
     puts "Nombre alumno: #{absent_student[0]}, Número de inasistencias: #{sum}" #aqui se muestra el Estudiante con el índice 0 que es donde están los nombres tiene "la suma" que es donde contó cada "A" o sea, cada inasistencia
-    end
-    end_line
   end
+  end_line
+end
 
 
+#opcion 3:  Imprimir en pantalla los nombres de los alumnos aprobados / Crear un método que reciba -como argumento- la nota necesaria para aprobar. Por defecto esa nota debe ser 5.0
+def name_approved
+  puts "Ingresa nota de aprobacion:"
+  nota=gets.chomp.to_i
 
+  student_list = read_alum('alumnos.csv')  #recorre el array de student_list
+  student_list.each do |average_list| #variable average_list
+    sum = 0
+    average_list.each_with_index do |student, index| #{} es una interpolacion
+      sum += student.to_f unless index == 0
+    end
+    if sum/5>=nota
+      puts "Alumno aprobado: #{average_list[0]}  con nota: #{sum/5}"
+    end
+  end
+  end_line
+end
 
 #Crea un menu de 4 opciones
 option = 0 #declaro una variable
